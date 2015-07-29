cc60101813e18c43a8a20346ef4e845ef2af94a1
Luke Dowell <lukedowell@dootdoot.local>
2015-07-29 10:52:02 -0500

    Basic web functionality

--- server/app.js
+++ server/app.js
@@ -1,3 +1,23 @@
 /**
  * Created by lukedowell on 7/29/15.
  */
+var express = require("express");
+var path = require("path");
+var app = express();
+
+//Set our port
+app.set('port', (process.env.PORT || 5000));
+
+//Base request
+app.get("/", function(req, res, next) {
+    //The below code will work, but does not account for more complex actions later on
+    //res.sendFile(__dirname + "/public/views/index.html");
+    //We use path to handle creating our absolute paths so as to avoid mix ups
+    res.sendFile(path.join(__dirname, "./public/views/index.html"));
+});
+
+//Create server
+//Callback function isn't necessary
+app.listen(app.get('port'), function() {
+   console.log("Hey! Listening on Port: " + app.get("port"));
+});
